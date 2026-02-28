4fd581abcbcb94ed680868e1ac4239896a580a7a
hojat72elect <hojat72elect@gmail.com>
2026-02-28 17:35:33 -0500

    A rudimentary UI for home page.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,20 @@
 import '../styles/Page.css'
 import {useEffect, useState} from "react";
 import type {Meal, RemoteRandomMeal} from "../data/RemoteRandomMeal.ts";
+import {RecipeCard} from "../components/RecipeCard.tsx";
+import type {Recipe} from "../schema.ts";
+import plusSignImage from "../assets/plusSign.png";
 
 
 export default function Home() {
-
+    const emptyRecipe: Recipe = {
+        id: "999",
+        name: 'New Recipe',
+        thumbnail: plusSignImage,
+        description: '',
+        ingredients: [],
+        instructions: []
+    }
 
     const [famousRecepes, setFamousRecepes] = useState<Meal[]>([]);
 
@@ -25,6 +35,20 @@ export default function Home() {
         <main className="page home-page">
             <h2>Home</h2>
             <p>Welcome — this is an empty home page.</p>
+            <div className="recipe-card-container">
+                {famousRecepes.map((recipe) => (
+                    <RecipeCard key={recipe.idMeal} {...{
+                        id: recipe.idMeal,
+                        name: recipe.strMeal,
+                        thumbnail: recipe.strMealThumb,
+                        description: '',
+                        ingredients: [],
+                        instructions: []
+                    }} />
+                ))}
+
+
+            </div>
         </main>
     )
 }
