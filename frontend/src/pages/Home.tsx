6f7064b540207c5f8f71f4640a9f8f2c7a0c6b60
hojat72elect <hojat72elect@gmail.com>
2026-02-28 16:55:44 -0500

    Made an API call for random meals in Home page.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,19 @@
 import '../styles/Page.css'
+import {useEffect} from "react";
+
 
 export default function Home() {
+
+
+    useEffect(()=>{
+        for (let counter =0; counter<10;counter++){
+            fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+                .then(response => response.json())
+                .then(data => console.log(data.meals[0]));
+        }
+
+    }, [])
+
   return (
     <main className="page home-page">
       <h2>Home</h2>
